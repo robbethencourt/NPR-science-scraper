485acd43172af1e5d2cb991fc6a17754906d6f79
Robert Bethencourt <robert.bethencourt@gmail.com>
2016-07-31 13:17:19 -0400

    articles being saved to db

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -1,6 +1,17 @@
 // Require request and cheerio. This makes the scraping possible
 var request = require('request');
-var cheerio = require('cheerio');;
+var cheerio = require('cheerio');
+
+
+// Database configuration
+var mongojs = require('mongojs');
+var databaseUrl = "nprnews";
+var collections = ["articles"];
+// Hook mongojs configuration to the db variable
+var db = mongojs(databaseUrl, collections);
+db.on('error', function(err) {
+  console.log('Database Error:', err);
+});
 
 module.exports = function(app) {
 	
@@ -15,11 +26,34 @@ module.exports = function(app) {
 
   				$('article').each(function(i, element) {
 
+  					var article = {};
+
   					var title = $(this).find('h2.title').text();
   					var href = $(this).find('a').attr('href');
   					var content = $(this).find('p.teaser').text();
-  					
-  					console.log(title, href, content);
+
+  					if (title !== '') {
+
+  						article = {
+	  						title: title,
+	  						href: href,
+	  						content: content,
+	  						comments: [{comment: ''}]
+	  					}
+	  					
+	  					// console.log(article);
+
+	  					db.articles.insert(article, function(err, saved) {
+	  						
+	  						if (err) {
+	  							throw err;
+	  						} else {
+	  							console.log(saved);
+	  						}
+
+	  					})
+
+  					}
 
   				});
 
--- server.js
+++ server.js
@@ -6,18 +6,6 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 
-// Database configuration
-var mongojs = require('mongojs');
-var databaseUrl = "nprnews";
-var collections = ["articles"];
-
-// Hook mongojs configuration to the db variable
-var db = mongojs(databaseUrl, collections);
-db.on('error', function(err) {
-  console.log('Database Error:', err);
-});
-
-
 // access to the public folder
 app.use(express.static('app/public'));
 
