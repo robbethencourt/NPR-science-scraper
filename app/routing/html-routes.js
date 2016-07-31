29d5aa0d185dbc55f9d17aa9d48dc94c72266975
Robert Bethencourt <robert.bethencourt@gmail.com>
2016-07-31 16:41:03 -0400

    articles on page with unique ids on data-role for each comment button

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -21,54 +21,94 @@ module.exports = function(app) {
 		// requesting the npr science news page
 		request('http://www.npr.org/sections/science/', function(error, response, html) {
 
-				if (error) throw error;
-			
-				// Load the html into cheerio and save it to a var
-  				var $ = cheerio.load(html);
-
-  				// loop through each article
-  				$('article').each(function(i, element) {
-
-  					// declare an empty object to pass to mongo
-  					var article = {};
-
-  					// grab the title, href and content of each article
-  					var title = $(this).find('h2.title').text();
-  					var href = $(this).find('a').attr('href');
-  					var content = $(this).find('p.teaser').text();
-
-  					// only add article elements that have content for a title
-  					if (title !== '') {
-
-  						// build the article object
-  						article = {
-	  						title: title,
-	  						href: href,
-	  						content: content,
-	  						comments: [{comment: ''}]
-	  					}
-	  					
-	  					// update the db with articles
-	  					db.articles.update(article, {upsert: true}, function(err, saved) {
-	  						
-	  						if (err) {
-	  							throw err;
-	  						} else {
-	  							console.log(saved);
-	  						}
-
-	  					})
-
-  					}
-
-  				});
-
-		});
+			if (error) throw error;
 		
-		res.render('index',  {
-			
-		});
+			// Load the html into cheerio and save it to a var
+			var $ = cheerio.load(html);
 
-	});
+			// loop through each article
+			$('article').each(function(i, element) {
+
+				// declare an empty object to pass to mongo
+				var article = {};
+
+				// grab the title, href and content of each article
+				var title = $(this).find('h2.title').text();
+				var href = $(this).find('a').attr('href');
+				var content = $(this).find('p.teaser').text();
+
+				// only add article elements that have content for a title
+				if (title !== '') {
+					// build the article object
+					article = {
+						title: title,
+						href: href,
+						content: content,
+						comments: []
+					}
+
+					// update the db with articles
+					db.articles.update(article, function(err, saved) {
+						
+						if (err) throw err;
+
+						console.log(saved);
+
+					}); // end db.articles.update()
+
+				} // end if
+
+			}); // end article.each()
+
+		}); // end request()
+
+		// find each of the articles in the database
+		db.articles.find({}, function(err, docs) {
+
+			if (err) throw err;
+
+			// articles and comments array to pass to handlebars
+			var articles_hb = [];
+			var comments_hb = [];
+
+			// article and comments objects to push to arrays listed above
+			var article_obj = {};
+			var comments_obj = {};
+
+			// loop through each of the returned docs from the database
+			docs.forEach(function(article, index, array) {
+
+				// emtpy out the object before each pass
+				article_obj = {};
+				comments_obj = {};
+
+				// build the article and comments objects
+				article_obj = {
+					title: article.title,
+					href: article.href,
+					content: article.content,
+					article_id: article._id.toString()
+				};
+				comments_obj = {
+					comment: article.comments
+				}
+
+				// push those created objects into the appropriate array
+				articles_hb.push(article_obj);
+				comments_hb.push(comments_obj);
+
+			}); // end docs.forEach()
+
+			// render the index page and pass the data to handlebars
+			res.render('index',  {
+
+				articles: articles_hb,
+				comments: comments_hb
+
+			}); // end res.render()
+			
+		}); // end db.articles.find()
+
+	}); // end app.get('/')
 
 } // end module.exports(app)
